c06b2019affef0fa00747cf2694b6faf4dbfe483
Drew <gooberpengi@gmail.com>
2015-07-14 05:11:45 -0400

    Attempting to make clicking "menu" toggle slidebar
    Added if/else to the menu click trigger

--- js/slidebar.js
+++ js/slidebar.js
@@ -1,14 +1,27 @@
+var toggle = true;
 var main = function() {
-  /* Push the body and the nav over by 285px over */
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
+	/* Push the body and the nav over by 285px over */
+	$('.icon-menu').click(function() {
+		if toggle = true (
+		  $('.menu').animate({
+			left: "0px"
+		  }, 200);
+		  $('body').animate({
+			left: "285px"
+		  }, 200);
+		  toggle = false;
+		)
+		else (
+		  $('.menu').animate({
+			left: "-285px"
+		  }, 200);
 
-    $('body').animate({
-      left: "285px"
-    }, 200);
-  });
+		  $('body').animate({
+			left: "0px"
+		  }, 200);
+		)
+		});
+		  
 
   /* Then push them back */
   $('.icon-close').click(function() {
