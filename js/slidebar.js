b096f27ece6574d8980dc878ec71e26cf7bd7472
Drew <gooberpengi@gmail.com>
2015-07-14 05:26:27 -0400

    Revert "Restoring to functional state"
    This reverts commit 22996333defd4f5b9f0f5be3b34aa8a6cfeca9ba.

--- js/slidebar.js
+++ js/slidebar.js
@@ -1,24 +1,41 @@
 var main = function() {
-  /* Push the body and the nav over by 285px over */
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
- 
-    $('body').animate({
-      left: "285px"
-    }, 200);
-  });
- 
+	/* Push the body and the nav over by 285px over */
+	var toggle = false;
+	$('.icon-menu').click(function() {
+		if toggle = false (
+		  $('.menu').animate({
+			left: "0px"
+		  }, 200);
+		  $('body').animate({
+			left: "285px"
+		  }, 200);
+		  toggle = true;
+		)
+		else if toggle = true (
+		  $('.menu').animate({
+			left: "-285px"
+		  }, 200);
+
+		  $('body').animate({
+			left: "0px"
+		  }, 200);
+		  toggle = false;
+		)
+	});
+		  
+
   /* Then push them back */
   $('.icon-close').click(function() {
     $('.menu').animate({
       left: "-285px"
     }, 200);
- 
+
     $('body').animate({
       left: "0px"
     }, 200);
+	toggle = false;
   });
 };
- 
+
+
+$(document).ready(main);
